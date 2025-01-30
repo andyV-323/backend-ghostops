5adbda8c3277f5475e7cfc1ff7c39cc0edd35c83
AndyV <you@example.com>
2025-01-29 17:58:51 -0800

    port 8080

--- server.js
+++ server.js
@@ -16,5 +16,5 @@ app.get("/", (req, res) => {
   res.send("GhostOps API is running");
 });
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
