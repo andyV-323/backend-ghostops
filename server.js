cbef47b7b05e16a5f93cf764a0374bb139c82fd5
AndyV <you@example.com>
2025-07-16 15:15:19 -0700

    back to previous

--- server.js
+++ server.js
@@ -14,15 +14,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-//app.use(cors());
-app.use(
-	cors({
-		origin: ["*"],
-		methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-		allowedHeaders: ["Content-Type", "Authorization"],
-		credentials: true,
-	})
-);
+app.use(cors());
 
 //Ensures cognito tokens are valid
 const verifier = CognitoJwtVerifier.create({
