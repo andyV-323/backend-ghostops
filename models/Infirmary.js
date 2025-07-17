857b2af970b35bfc2e571771c1c142d995cdb526
AndyV <you@example.com>
2025-07-16 17:34:19 -0700

    recoveryHours

--- models/Infirmary.js
+++ models/Infirmary.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose");
 
 const infirmarySchema = new mongoose.Schema({
-  createdBy: { type: String, required: true },
-  operator: { type: mongoose.Schema.ObjectId, ref: "Operator", default: null },
-  injuryType: { type: String, required: true },
-  recoveryDays: { type: Number, required: true },
-  injuredAt: { type: Date, default: Date.now, required: true },
+	createdBy: { type: String, required: true },
+	operator: { type: mongoose.Schema.ObjectId, ref: "Operator", default: null },
+	injuryType: { type: String, required: true },
+	recoveryHours: { type: Number, required: true },
+	injuredAt: { type: Date, default: Date.now, required: true },
 });
 
 module.exports = mongoose.model("Infirmary", infirmarySchema);
