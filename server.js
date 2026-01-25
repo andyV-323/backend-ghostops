4550ce91bd165b698fb1797c12886c06acc1f7fb
Andy <andyvalencia.cs@gmail.com>
2026-01-25 14:25:35 -0800

    Larger file Size

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const path = require("path");
 const app = express();
 
 app.use(express.json({ limit: "10mb" }));
-app.use(express.urlencoded({ limit: "10mb", extended: true }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(
 	cors({
 		origin: [
